c62b67561d6908af72112eec46fd09c9406cd4b1
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2025-01-21 17:43:35 +0100

    feat: implement ConnectButton component and enhance utility functions

---
+++ src/components/ConnectButton.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { useEffect, useRef } from "react";
+import {
+  useConnectModal,
+  useAccountModal,
+  useChainModal,
+} from "@rainbow-me/rainbowkit";
+import { useAccount, useDisconnect } from "wagmi";
+import { middleEllipsis } from "../utils";
+import { GambaUi } from "gamba-react-ui-v2";
+
+export const ConnectBtn = () => {
+  const { isConnecting, address, isConnected, chain } = useAccount();
+
+
+  const { openConnectModal } = useConnectModal();
+  const { openAccountModal } = useAccountModal();
+  const { openChainModal } = useChainModal();
+  const { disconnect } = useDisconnect();
+
+  const isMounted = useRef(false);
+
+  useEffect(() => {
+    isMounted.current = true;
+  }, []);
+
+  if (!isConnected) {
+    return (
+      <GambaUi.Button
+        onClick={async () => {
+          // Disconnecting wallet first because sometimes when is connected but the user is not connected
+          if (isConnected) {
+            disconnect();
+          }
+          openConnectModal?.();
+        }}
+        disabled={isConnecting}
+      >
+        { isConnecting ? 'Connecting...' : 'Connect your wallet' }
+      </GambaUi.Button>
+
+    );
+  }
+
+  if (isConnected && !chain) {
+    return (
+      <button className="btn" onClick={openChainModal}>
+        Wrong network
+      </button>
+    );
+  }
+
+  return (
+    <div className="max-w-5xl w-full flex items-center justify-between">
+      <div
+        className="flex justify-center items-center px-4 py-2 border border-neutral-700 bg-neutral-800/30 rounded-xl font-mono font-bold gap-x-2 cursor-pointer"
+        onClick={async () => openAccountModal?.()}
+      >
+        <div
+          role="button"
+          tabIndex={1}
+          className="h-8 w-8 rounded-full flex items-center justify-center flex-shrink-0 overflow-hidden"
+          style={{
+            boxShadow: "0px 2px 2px 0px rgba(81, 98, 255, 0.20)",
+          }}
+        >
+          {middleEllipsis(address ?? "", 4)}
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,24 @@
-import { GambaTransaction } from 'gamba-core-v2'
-import { GAMES } from './games'
+import { GambaTransaction } from "gamba-core-v2";
+import { GAMES } from "./games";
 
-export const truncateString = (s: string, startLen = 4, endLen = startLen) => s.slice(0, startLen) + '...' + s.slice(-endLen)
+export const truncateString = (s: string, startLen = 4, endLen = startLen) =>
+  s.slice(0, startLen) + "..." + s.slice(-endLen);
 
-export const extractMetadata = (event: GambaTransaction<'GameSettled'>) => {
+export const extractMetadata = (event: GambaTransaction<"GameSettled">) => {
   try {
-    const [version, ...parts] = event.data.metadata.split(':')
-    const [gameId] = parts
-    const game = GAMES.find((x) => x.id === gameId)
-    return { game }
+    const [version, ...parts] = event.data.metadata.split(":");
+    const [gameId] = parts;
+    const game = GAMES.find((x) => x.id === gameId);
+    return { game };
   } catch {
-    return {}
+    return {};
   }
-}
+};
+
+export const middleEllipsis = (str: string, len: number) => {
+  if (!str) {
+    return "";
+  }
+
+  return `${str.substr(0, len)}...${str.substr(str.length - len, str.length)}`;
+};
